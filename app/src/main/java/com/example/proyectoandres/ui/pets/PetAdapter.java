425cc395e6d90efd6999f1fe94c99f5133bc4210
andreslopez63 <andresetelsnchs@gmail.com>
2022-12-03 12:36:00 +0100

    borrado de mascotas

--- app/src/main/java/com/example/proyectoandres/ui/pets/PetAdapter.java
+++ app/src/main/java/com/example/proyectoandres/ui/pets/PetAdapter.java
@@ -1,19 +1,27 @@
 package com.example.proyectoandres.ui.pets;
 
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.proyectoandres.MainActivity;
 import com.example.proyectoandres.ModPetActivity;
 import com.example.proyectoandres.R;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.material.dialog.MaterialAlertDialogBuilder;
+import com.google.firebase.firestore.FirebaseFirestore;
 import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
@@ -22,8 +30,8 @@ public class PetAdapter extends RecyclerView.Adapter<PetAdapter.MyViewHolder> {
 
     Context context;
     ArrayList<Pet> petArrayList;
-
-
+    FirebaseFirestore db;
+    private static final String TAG = "PetAdapter";
     public PetAdapter(Context context, ArrayList<Pet> petArrayList) {
         this.context = context;
         this.petArrayList = petArrayList;
@@ -34,7 +42,7 @@ public class PetAdapter extends RecyclerView.Adapter<PetAdapter.MyViewHolder> {
     public PetAdapter.MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
 
         View v = LayoutInflater.from(context).inflate(R.layout.view_pets, parent, false);
-
+        db = FirebaseFirestore.getInstance();
         return new MyViewHolder(v);
     }
 
@@ -54,6 +62,51 @@ public class PetAdapter extends RecyclerView.Adapter<PetAdapter.MyViewHolder> {
 
 
         viewHolder.setOnClickListeners(pet.getUsuario(), pet.getIdmascota());
+
+        viewHolder.itemView.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+
+
+                new MaterialAlertDialogBuilder(context)
+
+                        .setMessage("¿Quieres borrar la mascota?")
+                        .setPositiveButton("Ok", /* listener = */ new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                Toast.makeText(context, "Hola soy topi"+pet.idmascota, Toast.LENGTH_SHORT).show();
+
+
+                                db.collection("usuarios").document(pet.usuario).collection("mascotas").document(pet.idmascota)
+                                        .delete()
+                                        .addOnSuccessListener(new OnSuccessListener<Void>() {
+                                            @Override
+                                            public void onSuccess(Void aVoid) {
+                                                Log.d(TAG, "DocumentSnapshot successfully deleted!");
+
+                                            }
+                                        })
+                                        .addOnFailureListener(new OnFailureListener() {
+                                            @Override
+                                            public void onFailure(@NonNull Exception e) {
+                                                Log.w(TAG, "Error deleting document", e);
+                                            }
+                                        });
+
+
+
+                                    petArrayList.remove(viewHolder.getAdapterPosition());
+
+
+                            }
+                        })
+                        .setNegativeButton("Cancel", /* listener = */ null)
+                        .show();
+
+
+                return true;
+            }
+        });
 
 
     }
